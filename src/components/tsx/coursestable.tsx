9df6e5d072cc2fbaea17294862d064ccb68fe324
Daniil211212 <daniilrodionov621@gmail.com>
2024-05-12 18:24:43 +0300

    first version

---
+++ src/components/tsx/coursestable.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/main.module.scss';
+
+const Coursetable = () => {
+    return(
+        <div className={styles}>
+            <body>
+                <div className={styles.page}>
+                    <div className={styles.nav}>
+                        sd
+                    </div>
+                </div>
+            </body>
+        </div>
+    );
+}
+
+export default Coursetable
