6cf5f88a251165776ddbc2f2d2ca48137391a780
Daniil211212 <daniilrodionov621@gmail.com>
2024-05-18 17:16:37 +0300

    two version

--- src/components/tsx/coursestable.tsx
+++ src/components/tsx/coursestable.tsx
@@ -1,4 +1,5 @@
 import styles from '../styles/main.module.scss';
+import logo from '../images/github-mark-white.png'
 
 const Coursetable = () => {
     return(
@@ -6,7 +7,15 @@ const Coursetable = () => {
             <body>
                 <div className={styles.page}>
                     <div className={styles.nav}>
-                        sd
+                        <h2>CRYPT CHECKER</h2>
+                    </div>
+                </div>
+
+
+
+                <div className={styles.footer}>
+                    <div className={styles.fotphoto}>
+                        <a href='https://github.com/drodionov0'><img src={logo} alt='git' width='30px'></img></a>
                     </div>
                 </div>
             </body>
